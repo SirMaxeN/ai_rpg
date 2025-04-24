5c9fb0935c4e3500431fbea4d10ba6e11120b8e2
SirMaxeN <maxenkom@gmail.com>
2025-04-24 19:26:28 +0200

    update commit script

--- commit.sh
+++ commit.sh
@@ -1,14 +1,33 @@
-echo "write commit message: "
+YELLOW="\033[1;33m"
+GREY="\033[1;30m"
+RED="\033[1;31m"
+GREEN="\033[1;32m"
+BLUE="\033[1;34m"
+PURPLE="\033[1;35m"
+CYAN="\033[1;36m"
+WHITE="\033[1;37m"
+NO_COLOR="\033[0m"
+
+echo "Write commit message: "
 read message
 echo
-echo "your message is: $message"
-read -p "Are you sure? (y/n) " -n 1 -r
+echo "Your message is: $message"
+echo -e "Are you sure? (${GREEN}y${NO_COLOR}/${RED}n${NO_COLOR}) "
+read -p "" -n 1 -r
 echo 
 if [[ $REPLY =~ ^[Yy]$ ]]
 then
-   echo "Doing commit..."
+   echo -e "Doing commit..."
    git add .
    git commit -m "$message"
    git push origin master
+   echo
+   echo 
+   echo -e "${GREEN}All done${NO_COLOR}"
+   echo
 fi
+echo
+echo
+echo -e "${RED}Commit abort${NO_COLOR}"
+echo
 
